42d69ce5c6f575d02a5ed62c20b62203433e6488
Carl Caum <carl@carlcaum.com>
2016-12-22 22:02:14 -0800

    Refactor PERequest class
    This commit refactors the PERequest class to be an abstract class that
    other classes can extend. That way different objects meant to interact
    with the PE APIs can all use a common transport to work from,
    dramatically simplifying the model.
    
    This commit also renames the `request` method to `send` and generally
    cleans things up.

--- src/main/java/org/jenkinsci/plugins/puppetenterprise/models/PERequest.java
+++ src/main/java/org/jenkinsci/plugins/puppetenterprise/models/PERequest.java
@@ -23,8 +23,8 @@ import java.lang.reflect.Type;
 import com.google.inject.Inject;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.internal.LinkedTreeMap;
-import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
+import com.google.gson.Gson;
 
 import org.jenkinsci.plugins.workflow.steps.AbstractStepDescriptorImpl;
 import org.jenkinsci.plugins.workflow.steps.AbstractStepImpl;
@@ -42,7 +42,8 @@ import org.apache.http.client.*;
 import org.apache.http.client.entity.*;
 import org.apache.http.entity.*;
 import org.apache.http.client.methods.*;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.*;
+import org.apache.http.conn.ssl.*;
 import org.apache.commons.io.IOUtils;
 
 import com.cloudbees.plugins.credentials.CredentialsMatchers;
@@ -58,19 +59,13 @@ import org.jenkinsci.plugins.puppetenterprise.models.PuppetEnterpriseConfig;
 import org.jenkinsci.plugins.puppetenterprise.models.PEResponse;
 import org.jenkinsci.plugins.workflow.PEException;
 
-public class PERequest {
+public abstract class PERequest {
   private static final Logger logger = Logger.getLogger(PERequest.class.getName());
   private String token = null;
   private Integer port = null;
   private String method = "GET";
   private Object body = new Object();
   private String endpoint = "";
-
-  public PERequest() { }
-
-  public PERequest(String token) { 
-    this.token = token;
-  }
 
   public String getToken() {
     return this.token;
@@ -132,19 +127,13 @@ public class PERequest {
     return HttpClients.custom().setSSLSocketFactory(sslsf).build();
   }
 
-  public final PEResponse request(String endpoint, Object body) throws Exception {
-    this.endpoint = endpoint;
+  public final PEResponse send(URI uri, Object body) throws Exception {
     this.method = "POST";
     this.body = body;
-    this.request();
+    return this.send(uri);
   }
 
-  public final PEResponse request(String endpoint) throws Exception {
-    this.endpoint = endpoint;
-    this.request();
-  }
-
-  public final PEResponse request() throws Exception {
+  public final PEResponse send(URI uri) throws Exception {
     Gson gson = new Gson();
     String responseString = "";
     Object responseBody = null;
@@ -159,21 +148,16 @@ public class PERequest {
       throw new Exception(e.getMessage());
     }
 
-    if (PuppetEnterpriseConfig.getPuppetMasterUrl() == null || PuppetEnterpriseConfig.getPuppetMasterUrl().isEmpty()) {
-      String message = "The Puppet Enterprise master address has not been configured yet. Configure the Puppet Enterprise page under Manage Jenkins.";
-      logger.log(Level.SEVERE, message);
-      throw new Exception(message);
-    }
-
     try {
       HttpResponse response = null;
 
       if (method.equals("POST")) {
-        HttpPost request = new HttpPost("https://" + PuppetEnterpriseConfig.getPuppetMasterUrl() + ":" + this.port + this.endpoint);
+        HttpPost request = new HttpPost(uri.toString());
 
         if (body != null) {
           request.addHeader("content-type", "application/json");
           request.addHeader("X-Authentication", accessToken);
+          System.out.println("Sending JSON: " + gson.toJson(this.body));
           StringEntity requestJson = new StringEntity(gson.toJson(this.body));
           request.setEntity(requestJson);
         }
@@ -181,22 +165,16 @@ public class PERequest {
       }
 
       if (method.equals("GET")) {
-        HttpGet request = new HttpGet("https://" + PuppetEnterpriseConfig.getPuppetMasterUrl() + ":" + this.port + this.endpoint);
+        HttpGet request = new HttpGet(uri.toString());
         request.addHeader("X-Authentication", accessToken);
         response = httpClient.execute(request);
       }
 
       String json = IOUtils.toString(response.getEntity().getContent());
+      System.out.println("RETURNED JSON: " + json);
       Integer responseCode = response.getStatusLine().getStatusCode();
 
-      try {
-        responseBody = gson.fromJson(json, Object.class);
-      } catch(JsonSyntaxException e) {
-        throw new PEException(e.getMessage());
-      }
-
-      peResponse = new PEResponse(responseBody, responseCode);
-
+      peResponse = new PEResponse(new Object(), responseCode, json);
     } catch(IOException e) {
       throw new PEException(e.getMessage());
     }
