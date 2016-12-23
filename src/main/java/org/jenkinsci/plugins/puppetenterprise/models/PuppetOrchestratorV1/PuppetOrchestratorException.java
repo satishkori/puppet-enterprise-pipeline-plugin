d603479113085b020661b8f1fa7844143a392e90
Carl Caum <carl@carlcaum.com>
2016-12-22 22:11:45 -0800

    Add working PuppetOrchestratorException class

--- src/main/java/org/jenkinsci/plugins/puppetenterprise/models/PuppetOrchestratorV1/PuppetOrchestratorException.java
+++ src/main/java/org/jenkinsci/plugins/puppetenterprise/models/PuppetOrchestratorV1/PuppetOrchestratorException.java
@@ -1,5 +1,31 @@
-class PuppetOrchestratorException extends PEException {
-  public PuppetOrchestratorException(String kind, String message, LinkedTreeMap details) {
-    super("Puppet orchestration failure: (" + kind + ") " + message + ". DETAILS: " + details.toString());
+package org.jenkinsci.plugins.puppetenterprise.models.puppetorchestratorv1;
+
+import com.google.gson.internal.LinkedTreeMap;
+
+public class PuppetOrchestratorException extends Exception {
+  private String kind = "";
+  private String message = "";
+  private LinkedTreeMap<String, Object> details = null;
+
+  public PuppetOrchestratorException(String kind, String message, LinkedTreeMap<String, Object> details) {
+    this.kind = kind;
+    this.message = message;
+    this.details = details;
+  }
+
+  public String getKind() {
+    return this.kind;
+  }
+
+  public String getMessage() {
+    return this.message;
+  }
+
+  public LinkedTreeMap<String, Object> getDetails() {
+    if (this.details == null) {
+      return new LinkedTreeMap();
+    }
+
+    return this.details;
   }
 }
