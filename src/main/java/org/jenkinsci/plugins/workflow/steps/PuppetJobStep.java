1d75e3233b9715672d8042a4b5eb5c619b1f4417
Carl Caum <carl@carlcaum.com>
2016-12-22 22:13:39 -0800

    Refactor PuppetJobStep
    This commit refactors the PuppetJobStep class to use the new PuppetJob
    class to create and monitor Puppet Enterprise Orchestrator jobs. This
    new way of managing everything is much cleaner and more maintainable.

--- src/main/java/org/jenkinsci/plugins/workflow/steps/PuppetJobStep.java
+++ src/main/java/org/jenkinsci/plugins/workflow/steps/PuppetJobStep.java
@@ -44,19 +44,25 @@ import com.google.gson.internal.LinkedTreeMap;
 
 import org.jenkinsci.plugins.puppetenterprise.PuppetEnterpriseManagement;
 import org.jenkinsci.plugins.puppetenterprise.models.PEResponse;
+import org.jenkinsci.plugins.puppetenterprise.models.PuppetJob;
+import org.jenkinsci.plugins.puppetenterprise.models.puppetorchestratorv1.PuppetOrchestratorException;
 import org.jenkinsci.plugins.workflow.PEException;
 
-public final class PuppetJobStep extends PuppetEnterpriseStep implements Serializable {
+public final class PuppetJobStep extends AbstractStepImpl implements Serializable {
 
-  private static final Logger logger = Logger.getLogger(PuppetJobStep.class.getName());
-
-  private String target = "";
-  private ArrayList nodes = new ArrayList();
-  private String application = "";
-  private String query = "";
+  private String target = null;
+  private ArrayList nodes = null;
+  private String application = null;
+  private String query = null;
   private Integer concurrency = null;
   private Boolean noop = false;
   private String environment = null;
+  private String credentialsId = "";
+
+  //TODO: Move this back to the PuppetEnterpriseStep class when done refactoring
+  @DataBoundSetter public void setCredentialsId(String credentialsId) {
+    this.credentialsId = Util.fixEmpty(credentialsId);
+  }
 
   @DataBoundSetter private void setTarget(String target) {
     this.target = Util.fixEmpty(target);
@@ -114,12 +120,20 @@ public final class PuppetJobStep extends PuppetEnterpriseStep implements Seriali
     return this.noop;
   }
 
-  @DataBoundConstructor public PuppetJobStep() { }
-
-  private static String parseJobId(String idUrl) {
-    String[] jobUrlElements = idUrl.split("/");
-    return jobUrlElements[jobUrlElements.length - 1];
+  //TODO: Move this back to the PuppetEnterpriseStep class when done refactoring
+  private String getToken() {
+    return lookupCredentials(this.credentialsId).getSecret().toString();
   }
+
+  //TODO: Move this back to the PuppetEnterpriseStep class when done refactoring
+  private static StringCredentials lookupCredentials(@Nonnull String credentialId) {
+    return CredentialsMatchers.firstOrNull(
+      CredentialsProvider.lookupCredentials(StringCredentials.class, Jenkins.getInstance(), ACL.SYSTEM, null),
+      CredentialsMatchers.withId(credentialId)
+    );
+  }
+
+  @DataBoundConstructor public PuppetJobStep() { }
 
   public static class PuppetJobStepExecution extends AbstractSynchronousStepExecution<Void> {
 
@@ -132,216 +146,38 @@ public final class PuppetJobStep extends PuppetEnterpriseStep implements Seriali
       justification = "Findbugs is wrong. The variable is not a dead store."
     )
     @Override protected Void run() throws Exception {
-      LinkedTreeMap scope = new LinkedTreeMap();
-      LinkedTreeMap body = new LinkedTreeMap();
+      PuppetJob job = new PuppetJob();
+      job.setConcurrency(step.getConcurrency());
+      job.setNoop(step.getNoop());
+      job.setEnvironment(step.getEnvironment());
+      job.setToken(step.getToken());
 
       // Target is still supported to support older versions of PE.
       // 2016.4 installs of PE should use the scope parameter when
       // creating orchestrator jobs.
       if (step.getTarget() != "" && step.getTarget() != null) {
-        body.put("target", step.getTarget());
+        job.setTarget(step.getTarget());
       } else {
-        if (step.getQuery() != "") {
-          scope.put("query", step.getQuery());
-        }
-
-        if (!step.getNodes().isEmpty()) {
-          scope.put("nodes", step.getNodes());
-        }
-
-        if (step.getApplication() != "") {
-          scope.put("application", step.getApplication());
-        }
-
-        if (!scope.isEmpty()) {
-          body.put("scope", scope);
-        }
+        job.setScope(step.getApplication(), step.getNodes(), step.getQuery());
       }
-
-      if (step.getConcurrency() != null) {
-        body.put("concurrency", step.getConcurrency());
-      }
-
-      body.put("noop", step.getNoop());
-      body.put("environment", step.getEnvironment());
-
-      PEResponse result = step.request("/orchestrator/v1/command/deploy", 8143, "POST", body);
-      LinkedTreeMap responseHash = (LinkedTreeMap) result.getResponseBody();
-
-      if (!step.isSuccessful(result)) {
-        String error = null;
-
-        if (responseHash.get("error") != null) {
-          if (responseHash.get("error") instanceof LinkedTreeMap) {
-            LinkedTreeMap errorHash = (LinkedTreeMap) responseHash.get("error");
-            error = errorHash.toString();
-          } else if (responseHash.get("error") instanceof String) {
-            String errorString = (String) responseHash.get("error");
-            error = errorString;
-          } else if (responseHash.get("error") instanceof ArrayList) {
-            ArrayList errorArray = (ArrayList) responseHash.get("error");
-            error = errorArray.toString();
-          }
-        }
-
-        //The orchestrator returns a msg key on error
-        if (responseHash.get("msg") != null) {
-          error = (String) responseHash.get("msg");
-        }
-
-        //The orchestrator return 404 on environment not found
-        if (result.getResponseCode() == 404 && error == null) {
-          error = "Environment " + step.getEnvironment() + " not found";
-        }
-
-        throw new PEException(error, result.getResponseCode(), listener);
-      }
-
-      LinkedTreeMap job = new LinkedTreeMap();
-      String jobID = "";
-      String jobStatus = "";
-      LinkedTreeMap jobStatusResponseHash = new LinkedTreeMap();
 
       try {
-        job = (LinkedTreeMap) responseHash.get("job");
-        jobID = (String) job.get("id");
-        jobStatus = "";
+        job.run();
+        listener.getLogger().println(job.formatReport());
+      } catch(PuppetOrchestratorException e) {
+        StringBuilder message = new StringBuilder();
+        message.append("Puppet Orchestrator Job Error\n");
+        message.append("Kind:    " + e.getKind() + "\n");
+        message.append("Message: " + e.getMessage() + "\n");
+        message.append("Details: " + e.getDetails().toString() + "\n");
 
-
-        listener.getLogger().println("Successfully created Puppet job " + parseJobId(jobID));
-        logger.log(Level.INFO, "Successfully created Puppet job " + parseJobId(jobID));
-      } catch(NullPointerException e){
-        throw new PEException(responseHash.toString(), 200);
+        throw new PEException(message.toString(), listener);
       }
-
-      do {
-        String peRequestPath = "/orchestrator/v1/" + jobID;
-        Integer peRequestPort = 8143;
-
-        // The orchestrator API in 2015.2 and 2016.1 returned
-        // a relative path for the job ID while 2016.2 returns
-        // a full URL. This code checks which was returned so
-        // we can support older PE installs.  This should eventually
-        // be deprecated.
-        try {
-          URI uri = new URI (jobID);
-          peRequestPath = uri.getPath();
-          peRequestPort = uri.getPort();
-        } catch(URISyntaxException e) { //do nothing
-        }
-
-        PEResponse jobStatusResponse = step.request(peRequestPath, peRequestPort, "GET", null);
-        jobStatusResponseHash = (LinkedTreeMap) jobStatusResponse.getResponseBody();
-
-        if (!step.isSuccessful(jobStatusResponse)) {
-          throw new PEException(jobStatusResponseHash.toString(), jobStatusResponse.getResponseCode());
-        }
-
-        ArrayList statuses = (ArrayList) jobStatusResponseHash.get("status");
-        LinkedTreeMap latestStatus = (LinkedTreeMap) statuses.get(statuses.size() - 1);
-        String currentState = (String) latestStatus.get("state");
-        jobStatus = currentState;
-
-        // Sleep for .5 seconds
-        try {
-          Thread.sleep(500);
-        } catch(InterruptedException ex) {
-          Thread.currentThread().interrupt();
-        }
-      } while (!jobStatus.equals("finished") && !jobStatus.equals("stopped") && !jobStatus.equals("failed"));
-
-      PEResponse nodes_response = step.request("/orchestrator/v1/jobs/" + parseJobId(jobID) + "/nodes", 8143, "GET", null);
-      jobStatusResponseHash.put("nodes", nodes_response.getResponseBody());
-      jobStatusResponseHash.put("status", jobStatus);
-
-      if (jobStatus.equals("failed") || jobStatus.equals("stopped")) {
-        String message = "Puppet job " + parseJobId(jobID) + " " + jobStatus + "\n---------\n" + step.formatReport(jobStatusResponseHash);
-        throw new PEException(message, listener);
-      }
-
-      String message = "Successfully ran Puppet job " + parseJobId(jobID) + "\n---------\n" + step.formatReport(jobStatusResponseHash);
-      listener.getLogger().println(message);
 
       return null;
     }
 
     private static final long serialVersionUID = 1L;
-  }
-
-  public String formatReport(LinkedTreeMap report) {
-    StringBuilder formattedReport = new StringBuilder();
-
-    Integer node_count = ((Double) report.get("node_count")).intValue();
-    String environment = (String) ((LinkedTreeMap) report.get("environment")).get("name");
-
-    formattedReport.append("Puppet Job Name: " + (String) report.get("name") + "\n");
-    formattedReport.append("Status: " + (String) report.get("status") + "\n");
-    formattedReport.append("Environment: " + environment + "\n");
-    formattedReport.append("Nodes: " + node_count.toString() + "\n\n");
-
-    ArrayList<LinkedTreeMap> nodes = (ArrayList) ((LinkedTreeMap) report.get("nodes")).get("items");
-    for (LinkedTreeMap node : nodes ) {
-      formattedReport.append(node.get("name") + "\n");
-
-      LinkedTreeMap node_details = (LinkedTreeMap) node.get("details");
-
-      if (node_details.get("metrics") != null) {
-        LinkedTreeMap metrics = (LinkedTreeMap) node_details.get("metrics");
-        Integer failed  = ((Double) metrics.get("failed")).intValue();
-        Integer changed = ((Double) metrics.get("changed")).intValue();
-        Integer skipped = ((Double) metrics.get("skipped")).intValue();
-        Integer corrective = null;
-
-        if (metrics.get("corrective_change") != null) {
-          corrective = ((Double) metrics.get("corrective_change")).intValue();
-        }
-
-        formattedReport.append("  Resource Events: ");
-        formattedReport.append(failed.toString() + " failed   ");
-        formattedReport.append(changed.toString() + " changed   ");
-
-        //PE versions prior to 2016.4 do not include corrective changes
-        if (corrective != null) {
-          formattedReport.append(corrective.toString() + " corrective   ");
-        }
-
-        formattedReport.append(skipped.toString() + " skipped    ");
-        formattedReport.append("\n");
-
-        formattedReport.append("  Report URL: " + node_details.get("report-url") + "\n");
-        formattedReport.append("\n");
-
-      } else {
-        //There's always a message, but it's only useful if the run was not able to take place,
-        //  which we'll know if there are no metrics.
-        if (node_details.get("message") != null) {
-          formattedReport.append(node_details.get("message") + "\n");
-          formattedReport.append("\n");
-        }
-      }
-
-    }
-
-    return formattedReport.toString();
-  }
-
-  public Boolean isSuccessful(PEResponse response) {
-    Integer responseCode = response.getResponseCode();
-    Object responseBody = response.getResponseBody();
-
-    if (responseCode < 200 || responseCode >= 300) {
-      return false;
-    }
-
-    if (responseBody instanceof LinkedTreeMap) {
-      LinkedTreeMap responseHash = (LinkedTreeMap) responseBody;
-
-      if (responseHash.get("error") != null) {
-        return false;
-      }
-    }
-
-    return true;
   }
 
   @Extension public static final class DescriptorImpl extends AbstractStepDescriptorImpl {
